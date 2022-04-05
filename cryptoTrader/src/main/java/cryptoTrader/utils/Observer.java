4a82836e8401728c2fdeee0460516d3ab34c5c41
Ansel Zeng <ansel.zeng.yp@gmail.com>
2022-04-04 22:22:32 -0400

    Swag 6

--- cryptoTrader/src/main/java/cryptoTrader/utils/Observer.java
+++ cryptoTrader/src/main/java/cryptoTrader/utils/Observer.java
@@ -1,5 +1,9 @@
 package cryptoTrader.utils;
 
+/**
+ * Public interface method
+ * used to implement the observer design pattern
+ */
 public interface Observer {
     public void update(Object o);
 }
